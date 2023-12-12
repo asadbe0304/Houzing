cea10bf9a17b9721e1562adcbc43f90b5a2c2101
xojiakbarakbarov <xakbarov508@gmail.com>
2023-12-12 22:53:53 +0500

    Ok

---
+++ src/pages/Properties/Properties.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Properties = () => {
+  return (
+    <div  className='text-center pt-14'>
+      <p className='text-3xl'> Properties</p>
+      <p className='text-base pt-4'>Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.</p>
+    </div>
+  )
+}
+
+export default Properties
