df58a355d5d18d7ef5bc3524d380fddcf9451ee6
xojiakbarakbarov <xakbarov508@gmail.com>
2023-12-12 22:50:58 +0500

    propertis and card icons

--- src/pages/Properties/index.jsx
+++ src/pages/Properties/index.jsx
@@ -1,8 +1,28 @@
 
 const index = () => {
+  let array=[1,2,3,4,5,6,7,8,9]
   return (
-    <div>
-      Hojiakbar
+    <div  className="container"> 
+      <div className="flex flex-col items-center  pt-16">
+          <p className="text-3xl font-semibold">Properties</p>
+       <p className="text-base font-normal pt-4 montserrat">Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.</p>
+      </div>
+      <div  className="pt-16">
+           <div className="flex justify-between"> 
+            <p>{array.length} results</p>
+            <div className="flex">
+              <p>Sort by:</p>
+              <select>
+                <option value="Newest Listings">Newest Listings</option>
+              </select>
+            </div>
+            <div className="flex justify-start gap-10 flex-wrap">
+                 <div>
+
+                 </div>
+            </div>
+           </div>
+      </div>
     </div>
   );
 };
