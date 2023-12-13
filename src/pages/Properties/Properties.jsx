9e79a91b43957269faa85170bf28b2d14ab924b0
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-12-13 11:42:20 +0500

    Proper

--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -1,11 +1,13 @@
 const Properties = () => {
   return (
-    <div className="text-center pt-14">
-      <p className="text-3xl"> Properties</p>
-      <p className="text-base pt-4">
-        Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
-      </p>
-    </div>
+    <>
+      <div className="text-center pt-14">
+        <p className="text-3xl"> Properties</p>
+        <p className="text-base pt-4">
+          Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.
+        </p>
+      </div>
+    </>
   );
 };
 
