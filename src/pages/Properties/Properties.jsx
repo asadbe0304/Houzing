bdefc74f54cee8ac9441b922eed57c16d405a963
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-12-13 13:01:28 +0500

    Erorr

--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -1,3 +1,4 @@
+
 const Properties = () => {
   return (
     <>
--- src/pages/Properties/index.jsx
+++ src/pages/Properties/index.jsx
@@ -1,12 +1,7 @@
 import Properties from "./Properties";
-<<<<<<< HEAD
-import Card from "Card.jsx";
-const index = () => {
-=======
 import Card from './Card.jsx'
 const index = () => {
 
->>>>>>> 09761233b01c898734c1fa41bf3c0f867f67dca5
   return (
     <div className="container">
       <Properties />
